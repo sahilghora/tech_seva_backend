49e8eed597515ae0c0768d72b9cdf0449c34a808
sahilghora <161731774+sahilghora@users.noreply.github.com>
2026-01-27 22:36:54 +0530

    Add model downloader for Render startup

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from utils.model_downloader import download_all_models
+download_all_models()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+import os
 
 # import routers
 from services.image_colorization.router import router as colorize_router
@@ -13,19 +17,16 @@ from services.diabetic_retinopathy.router import router as dr_router
 
 app = FastAPI(title="Unified ML Backend")
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://192.168.31.225:3000"],  # Next.js
+    allow_origins=["*"],  # frontend anywhere
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
+os.makedirs("results", exist_ok=True)
 app.mount("/results", StaticFiles(directory="results"), name="results")
-
-
 
 app.include_router(colorize_router, prefix="/api/colorize", tags=["Image Colorization"])
 app.include_router(stock_router, prefix="/api/stocks", tags=["Stock Prediction"])
@@ -34,7 +35,6 @@ app.include_router(fraud_tx_router, prefix="/api/fraud/transaction", tags=["Tran
 app.include_router(fraud_ins_router, prefix="/api/fraud/insurance", tags=["Insurance Fraud"])
 app.include_router(phishing_router, prefix="/api/phishing", tags=["Phishing Detection"])
 app.include_router(dr_router, prefix="/api/medical/dr", tags=["Diabetic Retinopathy"])
-
 
 @app.get("/")
 def root():
